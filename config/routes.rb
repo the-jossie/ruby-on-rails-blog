141c405f11e2c9465df9a40884357dd119a1b161
Dev Jo <idowuayanfeoluwa@gmail.com>
2024-02-12 09:40:07 +0100

    feat: added hierarchical routing for comment under resources

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   root "articles#index"
 
   resources :posts
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
