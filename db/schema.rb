41c24ade02ef758ff8c31dfd41e1d10379c64ce4
Dev Jo <idowuayanfeoluwa@gmail.com>
2024-02-12 08:11:23 +0100

    feat: migrate comments schema to database

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_02_10_205113) do
+ActiveRecord::Schema[7.0].define(version: 2024_02_11_105252) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string "commenter"
+    t.text "body"
+    t.integer "article_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["article_id"], name: "index_comments_on_article_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -25,4 +34,5 @@ ActiveRecord::Schema[7.0].define(version: 2024_02_10_205113) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "comments", "articles"
 end
